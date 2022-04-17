6fd813f4cd13e7485621a29fddd0c2acfeb29c55
Petar Kotsev <petarkocew@gmail.com>
2022-04-17 20:53:43 +0100

    add text and update button

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,4 +1,5 @@
 # Streamlit UI
+from random import choices
 import streamlit as st 
 # Keras load model
 from keras.models import load_model
@@ -10,7 +11,7 @@ import collections
 
 model = load_model('./models/newest.model')
 
-st.sidebar.title('TEST')
+st.sidebar.title('Selections')
 
 # load data for the selecets
 ## loading an existing workbook
@@ -29,26 +30,21 @@ terain_properties = list(map(lambda x: x.strip(), terain_properties))
 
 categories_sheet = wb['Sheet5']
 categories = list(categories_sheet.iter_cols(min_row=1, max_row=10, min_col=1, max_col=1,  values_only=True))[0]
+category_texts = list(categories_sheet.iter_cols(min_row=1, max_row=10, min_col=2, max_col=2,  values_only=True))[0]
 
 
 def changeHandler():
-    print(choices)
     l = {}
+    choices = st.session_state.choices + st.session_state.mult_choices
     predictions = getPredictions(choices, terain_properties)[0]
     for inx, clas in enumerate(categories):
         l[clas] = predictions[inx]
     new_l = {k: v for k, v in sorted(l.items(), key=lambda item: item[1], reverse=True)}
+    top_selection = list(new_l.keys())[0]
+    selection_inx = categories.index(top_selection)
+    string = category_texts[selection_inx].replace("[user's input strings]", str(choices)).replace("[probability of prediction]", str(new_l[top_selection]))
+    st.write(string)
     st.write(new_l)
-    
-
-choices = []
-for inx, clas in enumerate(terain_property_classes):
-    choices.append(st.sidebar.selectbox(clas, categorical_items[inx], key=clas, on_change=changeHandler))
-    if inx >=2:
-        break
-
-multy = st.sidebar.multiselect('Special features', np.concatenate(categorical_items[3:]), on_change=changeHandler)
-choices = choices + multy
 
 def getPredictions(predictionArr, properties):
     statements = np.zeros((1, len(properties)))
@@ -56,5 +52,18 @@ def getPredictions(predictionArr, properties):
         for prop in predictionArr:
             line[properties.index(prop)] = 1.0
     return model.predict(statements)
+
+
+st.session_state.choices = []
+
+for inx, clas in enumerate(terain_property_classes):
+    st.session_state.choices.append(st.sidebar.selectbox(clas, categorical_items[inx], key=clas))
+    if inx >=2:
+        st.session_state.mult_choices = st.sidebar.multiselect('Special features', np.concatenate(categorical_items[3:]))
+        break
+
+st.sidebar.button('Update', on_click=changeHandler)
+
+
 
 
